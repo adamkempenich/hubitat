384c4e6127c356dfa67ecadcda41f6b2580662e3
Adam Kemepnich <skiieraj@gmail.com>
2018-10-15 21:02:01 -0500

    Update MagicHome Wifi — Bulb (RGBW).groovy

--- MagicHome Wifi — Bulb (RGBW).groovy
+++ MagicHome Wifi — Bulb (RGBW).groovy
@@ -13,14 +13,14 @@ metadata {
         capability "Color Temperature"
         capability "Color Control"
         
-        command "on"
+        command "on"    
         command "off" 
 
         command "setLevel", [ "number" ]        // 0 - 100
         command "setHue", [ "number" ]          // 0 - 100
         command "setSaturation", [ "number" ]   // 0 - 100
         command "setAdjustedColor"
-        command "setColor", ["number", "number", "number"] // Hue (0-100), Saturation (0-100), Value (0-100)
+        command "setColor" // Hue (0-100), Saturation (0-100), Value (0-100)
         command "setColorTemperature", [ "number" ] // Kelvin ( Light Minimum Color Temperature - Light Maximum Color Temperature )
         command "setWhiteLevel", [ "number" ] // 0 - 100
 
