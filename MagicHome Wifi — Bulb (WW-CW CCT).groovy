12b1cb2fd29bbd1515bb754e60ad61e58b1866fc
Adam Kemepnich <skiieraj@gmail.com>
2019-02-20 14:04:05 -0600

    Update MagicHome Wifi — Bulb (WW-CW CCT).groovy
    Missed part of the code during the last upload

--- MagicHome Wifi — Bulb (WW-CW CCT).groovy
+++ MagicHome Wifi — Bulb (WW-CW CCT).groovy
@@ -8,7 +8,7 @@
  *
  *  Changelog:
  *
- *    0.81 (Feb 19 2019)
+*     0.81 (Feb 19 2019)
  *      - Added try/catch to intialize() method
  *      - Cleaned up a bunch of extraneous code
  *      - Fixed a rounding error in parse
@@ -30,39 +30,45 @@
  *  for the specific language governing permissions and limitations under the License.
  *
  */
+
 import hubitat.helper.HexUtils
 import hubitat.device.HubAction
 import hubitat.helper.InterfaceUtils
 import hubitat.device.Protocol
 
 metadata {
-    definition (name: "MagicHome Wifi — Bulb (WW/CW CCT)", namespace: "MagicHome", author: "Adam Kempenich") {
-		capability "Switch Level"
-		capability "Actuator"
-		capability "Switch"
-		capability "Polling"
-		capability "Refresh"
-		capability "Sensor"
-		capability "Color Temperature"
-		capability "Initialize"
+    definition (
+        name: "MagicHome Wifi — Bulb (WW/CW CCT)", 
+        namespace: "MagicHome", 
+        author: "Adam Kempenich") {
+        
+        capability "Switch Level"
+        capability "Actuator"
+        capability "Switch"
+        capability "Polling"
+        capability "Refresh"
+        capability "Sensor"
+        capability "Color Temperature"
+        capability "Initialize"
 
-		command "on"
-		command "off" 
+        command "setWarmWhiteLevel", [ "number" ] // 0 - 100
+        command "setColdWhiteLevel", [ "number" ] // 0 - 100
 
-		command "setWarmWhiteLevel", [ "number" ] // 0 - 100
-		command "setColdWhiteLevel", [ "number" ] // 0 - 100
 
-		attribute "warmWhiteLevel", "number"
-		attribute "coldWhiteLevel", "number"
+        attribute "warmWhiteLevel", "number"
+        attribute "coldWhiteLevel", "number"
     }
     
     preferences {  
-        input "deviceIP", "text", title: "Server", description: "Device IP", required: true, defaultValue: "192.168.1.X"
-        input "devicePort", "number", title: "Port", description: "Device Port", required: true, defaultValue: 5577
+        input "deviceIP", "text", title: "Server", description: "Device IP (e.g. 192.168.1.X)", required: true, defaultValue: "192.168.1.X"
+        input "devicePort", "number", title: "Port", description: "Device Port (Default: 5577)", required: true, defaultValue: 5577
 
-        input(name:"powerOnWithChanges", type:"bool", title: "Turn on this light when settings change?",
-              description: "Makes devices behave like other switches.", defaultValue: true,
-              required: true, displayDuringSetup: true)
+        input(name:"logDebug", type:"bool", title: "Log debug information?",
+              description: "Logs raw data for debugging. (Default: Off)", defaultValue: false,
+              required: false, displayDuringSetup: true)
+        
+        input(name:"powerOnWithChanges", type:"bool", title: "Turn on this light when values change?",
+              defaultValue: true, required: true, displayDuringSetup: true)
 
         input(name:"deviceWWTemperature", type:"number", title: "Warm White rating of this light",
             description: "Temp in K (default 2700)", defaultValue: 2700,
@@ -78,8 +84,8 @@ def on() {
     // Turn on the device
 
     sendEvent(name: "switch", value: "on")
-    logDebug "Switch set to on"
-    byte[] data = [0x71, 0x23,  0x0F, 0xA3]
+    logDebug( "Switch set to on" )
+    byte[] data = [0x71, 0x23, 0x0F, 0xA3]
     sendCommand(data)
 }
 
@@ -87,20 +93,20 @@ def off() {
     // Turn off the device
 
     sendEvent(name: "switch", value: "off")
-    logDebug "Switch set to off"
-    byte[] data = [0x71, 0x24,  0x0F, 0xA4]
+    logDebug( "Switch set to off" )
+    byte[] data = [0x71, 0x24, 0x0F, 0xA4]
     sendCommand(data)
 }
 
 def setLevel(level, transmit=true) {
     // Set the brightness of a device (0-100)
 
-    normalizePercent(level)
+    level = normalizePercent( level )
     sendEvent(name: "level", value: level)
-	logDebug "Level set to ${level}"
-	
-	if( !transmit ) return level
-    setColorTemperature( device.currentValue('colorTemperature'), level )
+    logDebug( "Level set to ${level}")
+    
+    if( !transmit ) return level
+    setColor( level: level )
 }
 
 def setWarmWhiteLevel(warmWhiteLevel, transmit=true){
@@ -108,9 +114,9 @@ def setWarmWhiteLevel(warmWhiteLevel, transmit=true){
 
     normalizePercent(warmWhiteLevel)
     sendEvent(name: "warmWhiteLevel", value: warmWhiteLevel)
-	logDebug "Warm White Level set to ${warmWhiteLevel}"
-	
-	if( !transmit ) return warmWhiteLevel
+    logDebug( "Warm White Level set to ${warmWhiteLevel}")
+    
+    if( !transmit ) return warmWhiteLevel
     setColorTemperature(null)
 }
 
@@ -119,17 +125,19 @@ def setColdWhiteLevel(coldWhiteLevel, transmit=true){
 
     normalizePercent(coldWhiteLevel)
     sendEvent(name: "coldWhiteLevel", value: coldWhiteLevel)
-	logDebug "Cold White Level set to ${coldWhiteLevel}"
+    logDebug("Cold White Level set to ${coldWhiteLevel}")
     if( !transmit ) return coldWhiteLevel
     setColorTemperature(null)
 }
 
-def setColorTemperature(setTemp = device.currentValue('colorTemperature'), deviceLevel = device.currentValue('level'), transmit=true){
-    // Adjust the color temperature of a device
-	
-    sendEvent(name: "colorTemperature", value: setTemp)
-	logDebug "Color Temperature set to ${setTemp}"
+
+
+def setColorTemperature( setTemp = device.currentValue('colorTemperature'), deviceLevel = device.currentValue('level'), transmit=true ){
+    // Adjust the color temperature of a device  
     
+    sendEvent(name: "colorTemperature", value: setTemp)
+    logDebug "Color Temperature set to ${setTemp}"
+
     brightnessWW = proportionalToDeviceLevel(invertLinearValue( setTemp, settings.deviceWWTemperature, settings.deviceCWTemperature ) )
     brightnessCW = proportionalToDeviceLevel(invertLinearValue( setTemp, settings.deviceCWTemperature, settings.deviceWWTemperature ) )
 
@@ -138,13 +146,11 @@ def setColorTemperature(setTemp = device.currentValue('colorTemperature'), devic
         brightnessCW = brightnessCW / (( brightnessWW + brightnessCW ) / 100 )
     }
 
-    setWarmWhiteLevel( brightnessWW, false )
-    setColdWhiteLevel( brightnessCW, false )
+    sendEvent(name: "warmWhiteLevel", value: brightnessWW)
+    sendEvent(name: "warmWhiteLevel", value: brightnessCW)
 
-    // Update Device
-	if( !transmit ) return setTemp
-	
-	byte[] data =  powerOnWithChanges(true) + appendChecksum([ 0x31, brightnessWW * 2.55, brightnessCW * 2.55, 0x00, 0x03, 0x01, 0x0f ] ) 
+    if( !transmit ) return setTemp
+    byte[] data = powerOnWithChanges(true) + appendChecksum( [ 0x31, brightnessWW * 2.55, brightnessCW * 2.55, 0x00, 0x03, 0x01, 0x0f ] )
     sendCommand( data )
 }
 
@@ -153,26 +159,11 @@ def setColorTemperature(setTemp = device.currentValue('colorTemperature'), devic
 def powerOnWithChanges( append=false ){
     // If the device is off and light settings change, turn it on (if user settings apply)
     if(append){
-        return settings.powerOnWithChanges ? ( [0x71, 0x23, 0x0F, 0xA3] ) : ([])
+        return settings.powerOnBrightnessChange ? ( [0x71, 0x23, 0x0F, 0xA3] ) : ([])
     }
     else{
-        settings.powerOnWithChanges ? ( device.currentValue("status") != "on" ? on() : null ) : null
+        settings.powerOnBrightnessChange ? ( device.currentValue("status") != "on" ? on() : null ) : null
     }
-}
-
-def appendChecksum( data ){
-    // Adds a checksum to an array
-    
-    data += calculateChecksum(data)
-    return data 
-}
-
-def normalizePercent( value, lowerBound = 0, upperBound = 100, fallBack = upperBound ){
-    // Takes a value and ensures it's between two defined thresholds
-    
-    if( value == null ) return fallBack
-    lowerBound <= upperBound ? ( null ) : ( ( lowerBound, upperBound ) = [ upperBound, lowerBound] )
-    return value < upperBound ? ( value > lowerBound ? ( value ) : ( lowerBound ) ) : ( upperBound ) 
 }
 
 def invertLinearValue( neutralValue, value1, value2 ){
@@ -182,8 +173,8 @@ def invertLinearValue( neutralValue, value1, value2 ){
 }
 
 def proportionalToDeviceLevel( value ){
-	// Returns the value of a number proportionally to the device's brightness
-	
+    // Returns the value of a number proportionally to the device's brightness
+    
     return roundUpBetweenZeroAndOne( normalizePercent( value * device.currentValue('level') / 100 ) )
 }
 
@@ -193,13 +184,113 @@ def roundUpBetweenZeroAndOne(number){
     return number > 0 && number < 1 ? ( 1 ) : ( number )
 }
 
-def calculateChecksum(bytes){
+
+def calculateCTSaturation( coldWhite = true, offset ) {
+        
+    def CURVE
+    def lowPoint
+    def highPoint
+    
+    if( coldWhite ) {
+        lowPoint = settings.cwSaturationLowPoint < settings.cwSaturationHighPoint ? settings.cwSaturationLowPoint : settings.cwSaturationHighPoint
+        highPoint = settings.cwSaturationHighPoint > settings.cwSaturationLowPoint ? settings.cwSaturationHighPoint : settings.cwSaturationLowPoint
+        CURVE = 1.8
+    }
+    else{ 
+        lowPoint = settings.wwSaturationLowPoint < settings.wwSaturationHighPoint ? settings.wwSaturationLowPoint : settings.wwSaturationHighPoint
+        highPoint = settings.wwSaturationHighPoint > settings.wwSaturationLowPoint ? settings.wwSaturationHighPoint : settings.wwSaturationLowPoint
+        CURVE = 2.16666
+    }
+    
+    return (((( 100 - lowPoint  ) / 100 ) * ( CURVE * Math.sqrt( offset ))) + lowPoint  ) * highPoint / 100
+}      
+
+def hslToCT(){
+
+}
+
+def normalizePercent(value, lowerBound=0, upperBound=100 ){
+    // Takes a value and ensures it's between two defined thresholds
+    
+    // If the value doesn't exist, create it
+    value = value == null ? upperBound : value
+
+    if(lowerBound < upperBound){
+        if(value < lowerBound ){ value = lowerBound}
+        if(value > upperBound){ value = upperBound}
+    }
+    else if(upperBound < lowerBound){
+        if(value < upperBound){ value = upperBound}
+        if(value > lowerBound ){ value = lowerBound}
+    }
+    
+    return value
+}
+
+def checkIfInMap( parameterValue, valueName) {
+    // Check if a value is in a map, and return (or set and return) a value
+
+    if( parameterValue == null ){ 
+        if( device.currentValue( valueName ) == null ){
+            sendEvent( name: valueName, value: maxValue )
+            return device.currentValue( "${valueName}" )
+        }
+        else{
+            return device.currentValue( "${valueName}" )
+        }
+    }
+    else {
+        sendEvent( name: valueName, value: parameterValue )
+        return parameterValue
+    }
+}
+
+def calculateChecksum( data ){
     // Totals an array of bytes
     
-    int sum = 0
-    for(int d : bytes)
+    int sum = 0;
+    for(int d : data)
         sum += d;
     return sum & 255
+}
+
+def appendChecksum( data ){
+    // Adds a checksum to an array
+    
+    data += calculateChecksum(data)
+    return data 
+}
+
+def parse( response ) {
+    // Parse data received back from this device
+
+    def responseArray = HexUtils.hexStringToIntArray(response)
+    if( responseArray.length == 4 ) {
+        // Does the device say it's on?
+        
+        responseArray[ 2 ] == 35 ? sendEvent(name: "switch", value: "on") : sendEvent(name: "switch", value: "off")
+    }
+    else if( responseArray.length == 14 ) {
+        // Does the device say it's on?
+        
+        responseArray[ 2 ] == 35 ? ( sendEvent(name: "switch", value: "on") ) : ( sendEvent(name: "switch", value: "off") )
+        
+        // Convert integers to percentages
+        def warmWhite = ( responseArray[ 6 ].toDouble() / 2.55 ).round()
+        def coldWhite = ( responseArray[ 7 ].toDouble() / 2.55 ).round()
+        
+        // If values differ from HE, change them
+        sendEvent( name: 'warmWhiteLevel', value: warmWhite  )
+        sendEvent( name: 'coldWhiteLevel', value: coldWhite  )
+        sendEvent( name: 'level', value: warmWhite + coldWhite )
+        sendEvent( name: "colorTemperature", value: (settings.deviceCWTemperature - (( settings.deviceCWTemperature - settings.deviceWWTemperature ) * ( warmWhite / 100 ))).toInteger() )
+    }
+    else if( response == null ){
+        logDebug "No response received from device" 
+    }
+    else{
+        logDebug "Received a response with an unexpected length of " + responseArray.length
+    }
 }
 
 private logDebug( debugText ){
@@ -240,20 +331,21 @@ def poll() {
 def updated(){
     initialize()
 }
+
 def initialize() {
     // Establish a connection to the device
     
     logDebug "Initializing device."
-	telnetClose()
-	try {
-		logDebug("Opening TCP-Telnet Connection.")
-	    InterfaceUtils.socketConnect(device, settings.deviceIP, settings.devicePort.toInteger(), byteInterface: true)
-		
-		pauseExecution(1000)
-		logDebug("Connection successfully established")
-	} catch(e) {
-		logDebug("Error attempting to establish TCP-Telnet connection to device.")
-	}
+    telnetClose()
+    try {
+        logDebug("Opening TCP-Telnet Connection.")
+        InterfaceUtils.socketConnect(device, settings.deviceIP, settings.devicePort.toInteger(), byteInterface: true)
+        
+        pauseExecution(1000)
+        logDebug("Connection successfully established")
+    } catch(e) {
+        logDebug("Error attempting to establish TCP-Telnet connection to device.")
+    }
     unschedule()
 
     runIn(20, keepAlive)
@@ -263,6 +355,5 @@ def keepAlive(){
     // Poll the device every 250 seconds, or it will lose connection.
     
     refresh()
-	unschedule()
-    runIn(10, keepAlive)
+    runIn(150, keepAlive)
 }
