05feebf145053184caed8d4f440564a1720556c0
Adam Kemepnich <skiieraj@gmail.com>
2019-04-12 20:48:07 -0500

    Update MagicHome Wifi — Bulb (Dimmable).groovy

--- Drivers/MagicHome/MagicHome Wifi — Bulb (Dimmable).groovy
+++ Drivers/MagicHome/MagicHome Wifi — Bulb (Dimmable).groovy
@@ -1,38 +1,54 @@
 /**
- *  MagicHome Wifi - Bulb (Dimmable) 0.83
- *
- *  Author: 
- *    Adam Kempenich 
- *
- *  Documentation:  https://community.hubitat.com/t/release-beta-0-7-magic-home-wifi-devices-initial-public-release/5197
- *
- *  Changelog:
- *
- *	0.83 (Feb 28 2019) 
- *	  - Un-did the parse() removal. Added Data checking for parse()
- *	  - Added a setting for time-to-re
- *
- *  	0.82 (Feb 25 2019)
- *	  - Commented out parse() contents, since I think they are causing slowdown...
- *	  0.81 (Feb 19 2019) 
- *      - Added try/catch to initialize() method
- * 
- *    0.8 (Feb 11 2019)
- *      - Initial Release
- *      - Cannot set device time, yet.
- *      - Auto-discovery not yet implemented
- *      - On-device scheduling not yet implemented
- *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License. You may obtain a copy of the License at:
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
- *  for the specific language governing permissions and limitations under the License.
- *
- */
+*  MagicHome Wifi - Bulb (Dimmable) 0.85
+*
+*  Author: 
+*    Adam Kempenich 
+*
+*  Documentation:  https://community.hubitat.com/t/release-beta-0-7-magic-home-wifi-devices-initial-public-release/5197
+*
+*  Changelog:
+*
+*	0.85 (April 12 2019)
+*		- Simplified most of the code
+*		- Eliminated Telnet method
+*		- Removed most of parse() while I simplify it further. Only power reports.
+*		- Continued to enhance self-healing. Devices that are physically powered off report as off.
+*		- Reworked powerOnWithChanges.
+* 		- Added import URL
+*
+*	0.84 (Mar 28 2019) 
+* 		- Completely reworked Telnet and Socket methods... Waiting on some bugfixes
+* 		- Greatly enhanced self-healing capabilities
+*
+*	0.83 (Feb 27 2019) 
+*	  - Un-did the parse() removal. Added Data checking for parse()
+*	  - Added a setting for time-to-refresh
+*
+*  	0.82 (Feb 25 2019)
+*	  - Commented out parse() contents, since I think they are causing slowdown...
+*
+*    0.81 (Feb 19 2019) 
+* 		- Added try/catch to initialize method
+* 		- Removed a bunch of extraneous code
+* 		- Fixed an issue with data loss for CCT in parse method 
+*
+*    0.8 (Feb 11 2019)
+*      - Initial Release
+*      - Cannot set device time, yet.
+*      - Auto-discovery not yet implemented
+*      - Custom functions not yet implemented
+*      - On-device scheduling not yet implemented
+*
+*  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License. You may obtain a copy of the License at:
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+*  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+*  for the specific language governing permissions and limitations under the License.
+*
+*/
 
 import hubitat.helper.HexUtils
 import hubitat.device.HubAction
@@ -43,16 +59,17 @@ metadata {
     definition (
         name: "MagicHome Wifi — Bulb (Dimmable)", 
         namespace: "MagicHome", 
-        author: "Adam Kempenich") {
+        author: "Adam Kempenich",
+		importUrl: "https://raw.githubusercontent.com/adamkempenich/hubitat/master/Drivers/MagicHome/MagicHome%20Wifi%20—%20Bulb%20(Dimmable).groovy") {
         
+        capability "Switch Level"
         capability "Actuator"
-        capability "Initialize"
+        capability "Switch"
         capability "Polling"
         capability "Refresh"
-        capability "Switch"
-        capability "Switch Level"
-
-    }
+        capability "Sensor"
+        capability "Initialize"
+	}
     
     preferences {  
         input "deviceIP", "text", title: "Server", description: "Device IP (e.g. 192.168.1.X)", required: true, defaultValue: "192.168.1.X"
@@ -64,52 +81,38 @@ metadata {
         
         input(name:"powerOnWithChanges", type:"bool", title: "Turn on this light when values change?",
               defaultValue: true, required: true, displayDuringSetup: true)
-	    
-	input(name:"refreshTime", type:"number", title: "Time to refresh (seconds)",
+
+		input(name:"refreshTime", type:"number", title: "Time to refresh (seconds)",
             description: "Interval between refreshing a device for its current value. Default: 60. Recommended: Under 200", defaultValue: 60,
             required: true, displayDuringSetup: true)
     }
 }
-
 def on() {
     // Turn on the device
 
     sendEvent(name: "switch", value: "on")
-    logDebug( "Switch set to on" )
+    logDebug "Switch set to on" 
     byte[] data = [0x71, 0x23, 0x0F, 0xA3]
     sendCommand(data)
 }
 
-def off() {
-    // Turn off the device
-
-    sendEvent(name: "switch", value: "off")
-    logDebug( "Switch set to off" )
-    byte[] data = [0x71, 0x24, 0x0F, 0xA4]
-    sendCommand(data)
-}
-
-def setLevel(level, transmit=true) {
+def setLevel(level) {
     // Set the brightness of a device (0-100)
-
+	level > 100 ? (level = 100) : null
     sendEvent(name: "level", value: level)
-    logDebug( "Level set to ${level}")
-    
-    if( !transmit ) return level
-    byte[] data = powerOnWithChanges(true) + appendChecksum( [ 0x31, level * 2.55, 0, 0, 0x03, 0x01, 0x0f ] )
+    logDebug "Level set to ${level}"
+    powerOnWithChanges()
+	
+	byte[] data = appendChecksum([0x31, level*2.55, 0, 0, 0x03, 0x01, 0x0f])
     sendCommand( data ) 
 }
 
 // ------------------- Helper Functions ------------------------- //
 
-def powerOnWithChanges( append=false ){
+def powerOnWithChanges( ){
     // If the device is off and light settings change, turn it on (if user settings apply)
-    if(append){
-        return settings.powerOnBrightnessChange ? ( [0x71, 0x23, 0x0F, 0xA3] ) : ([])
-    }
-    else{
-        settings.powerOnBrightnessChange ? ( device.currentValue("status") != "on" ? on() : null ) : null
-    }
+
+	settings.powerOnWithChanges ? ( device.currentValue("status") != "on" ? on() : null ) : null
 }
 
 def calculateChecksum( data ){
@@ -129,17 +132,20 @@ def appendChecksum( data ){
 }
 
 def parse( response ) {
-//    // Parse data received back from this device
-//
-	def responseArray = HexUtils.hexStringToIntArray(response)	
+    // Parse data received back from this device
+	
+	unschedule()
+	runIn(20, refresh)
+	
+    def responseArray = HexUtils.hexStringToIntArray(response)	
 	switch(responseArray.length) {
 		case 4:
 			logDebug( "Received power-status packet of ${response}" )
 			if( responseArray[2] == 35 ){
-				device.currentValue( 'status' ) != 'on' ? sendEvent(name: "switch", value: "on") : null
+				sendEvent(name: "switch", value: "on")
 			}
 			else{
-				device.currentValue( 'status' ) != 'off' ? sendEvent(name: "switch", value: "off") : null
+				sendEvent(name: "switch", value: "off")
 			}
 			break;
 		
@@ -147,25 +153,19 @@ def parse( response ) {
 			logDebug( "Received general-status packet of ${response}" )
 		
 			if( responseArray[2] == 35 ){
-				device.currentValue( 'status' ) != 'on' ? sendEvent(name: "switch", value: "on") : null
+				sendEvent(name: "switch", value: "on")
 			}
 			else{
-				device.currentValue( 'status' ) != 'off' ? sendEvent(name: "switch", value: "off") : null
+				sendEvent(name: "switch", value: "off")
 			}
-		
-			def level = ( responseArray[ 6 ].toDouble() / 2.55 ).round()
-			device.currentValue( 'level' ) != level ? sendEvent( name: "level", value: level ) : null
-			
-			break;
-		
-		case null:
+		break;	
+	case null:
 			logDebug "No response received from device"
 			initialize()
-			break;
-		
-		default:
-			logDebug "Received a response with an unexpected length of ${responseArray.length} containing ${response}"
-			break;
+			break;	
+	default:
+		logDebug "Received a response with an unexpected length of ${responseArray.length} containing ${response}"
+		break;
 	}
 }
 
@@ -179,25 +179,24 @@ private logDebug( debugText ){
 
 def sendCommand( data ) {
     // Sends commands to the device
-    
-    String stringBytes = HexUtils.byteArrayToHexString(data)
-    logDebug "Transmitting: ${stringBytes}"
-    InterfaceUtils.sendSocketMessage(device, stringBytes)
-}
-def refresh( ) {
-    
-    byte[] data =  [ 0x81, 0x8A, 0x8B, 0x96 ]
-    sendCommand( data )
+	unschedule()
+	runIn(60, initialize)
+
+
+	String stringBytes = HexUtils.byteArrayToHexString(data)
+	logDebug "${data} was converted. Transmitting: ${stringBytes}"
+	InterfaceUtils.sendSocketMessage(device, stringBytes)
 }
 
-def telnetStatus( status ) { logDebug "telnetStatus: ${status}" }
+def refresh( ) {
+    byte[] data =  [0x81, 0x8A, 0x8B, 0x96 ]
+    sendCommand(data)
+}
+
 def socketStatus( status ) { 
-    logDebug "socketStatus: ${status}"
-    if(status == "send error: Broken pipe (Write failed)" || status == "send error: Connection timed out (Write failed)") {
-        // Cannot reach device
-        logDebug "Cannot reach device. Attempting to reconnect."
-        runIn(2, initialize)
-    }   
+	logDebug "socketStatus: ${status}"
+	logDebug "Attempting to reconnect in 10 seconds."
+	runIn(10, initialize)
 }
 
 def poll() {
@@ -212,25 +211,24 @@ def initialize() {
     // Establish a connection to the device
     
     logDebug "Initializing device."
-	telnetClose()
+	
+	InterfaceUtils.socketClose(device)
+	unschedule()
 	try {
-		logDebug("Opening TCP-Telnet connection.")
-	    InterfaceUtils.socketConnect(device, settings.deviceIP, settings.devicePort.toInteger(), byteInterface: true)
-		
+		logDebug "Opening Socket Connection."
+		InterfaceUtils.socketConnect(device, settings.deviceIP, settings.devicePort.toInteger(), byteInterface: true)
 		pauseExecution(1000)
-		logDebug("Connection successfully established")
+		logDebug "Connection successfully established"
+	    runIn(1, refresh)
 	} catch(e) {
-		logDebug("Error establishing TCP-Telnet connection.")
+		logDebug("Error attempting to establish TCP connection to device.")
+		logDebug("Next initialization attempt in 20 seconds.")
+		sendEvent(name: "switch", value: "off") // If we didn't hear back, the device is likely physically powered off
+		runIn(20, initialize)
 	}
-    unschedule()
-
-    runIn(5, keepAlive)
 }
 
-def keepAlive(){
-    // Poll the device every 250 seconds, or it will lose connection.
-    
-    refresh()
-	unschedule()
-	settings.refreshTime == null ? runIn(60, keepAlive) : runIn(settings.refreshTime, keepAlive)
+def installed(){
+	sendEvent(name: "level", value: 100)
+	sendEvent(name: "switch", value: "off")
 }
