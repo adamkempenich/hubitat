aa058707ba1b092bb53b63880223031175447d7a
Adam Kempenich <skiieraj@gmail.com>
2020-02-15 21:49:02 -0600

    0.22

--- Drivers/Lightify/(Child) Lightify Bulb - RGBW.groovy
+++ Drivers/Lightify/(Child) Lightify Bulb - RGBW.groovy
@@ -1,5 +1,5 @@
 /**
-* Lightify Bulb - RGBW (0.21)
+* Lightify Child - RGBW (0.22)
 *
 *  Author: 
 *    Adam Kempenich
@@ -7,6 +7,8 @@
 *  Documentation:  [Does not exist, yet]
 *
 *  Changelog:
+*    0.22 (Feb 15, 2020)
+*	- Updated naming schema
 *
 *    0.21 (Feb 10, 2020)
 *        - Fixed number formatting
@@ -36,10 +38,10 @@
 
 metadata {
 definition (
-    name: "Lightify Bulb - RGBW", 
+    name: "Lightify Child - RGBW", 
     namespace: "Lightify", 
     author: "Adam Kempenich",
-    importUrl: "https://raw.githubusercontent.com/adamkempenich/hubitat/master/Drivers/Lightify/(Child)%20Lightify%20Bulb%20-%20RGBW.groovy") {
+    importUrl: "https://raw.githubusercontent.com/adamkempenich/hubitat/master/Drivers/Lightify/(Child)%20Lightify%20Child%20-%20RGBW.groovy") {
     
         capability "Actuator"
         capability "Color Control"
@@ -52,53 +54,138 @@ definition (
 		capability "Switch"
 		capability "Switch Level"
 	}
+    preferences {  
+         input(name:"logDescriptionText", type:"bool", title: "Log descriptionText?",
+              description: "Logs when things happen. (Default: On)", defaultValue: true,
+              required: true, displayDuringSetup: true)
+  
+        input(name:"enablePreStaging", type:"bool", title: "Enable Color Pre-Staging?",
+              defaultValue: false, required: true, displayDuringSetup: true)
+    }
 }
 
 def on(){
+    // Turn the device on
+    
     sendEvent(name: "switch", value: "on")
     parent.on(device.deviceNetworkId)
 }
 def off(){
+    // Turn the device off
+    
     sendEvent(name: "switch", value: "on")
     parent.off(device.deviceNetworkId)
 }
-def setLevel(brightness, duration=0){
-    sendEvent(name: "level", value: brightness.toInteger())
-    parent.setLevel(device.deviceNetworkId, brightness.toInteger())   
+def setLevel(levelValue, duration=0){
+    // Update the brightness of  adevice 
+    
+    sendEvent(name: "level", value: levelValue.toInteger())
+    parent.setLevel(device.deviceNetworkId, levelValue.toInteger())   
 }
 
 def setHue(hueValue){
+    // Update only the hue of a device
+    
+    clamp(hueValue)
+    
+    logDescriptionText("Hue set to ${hueValue}")
+
+
     sendEvent(name: "saturation", value: hueValue.toFloat())
     def parameters = [hue: device.currentValue('hue'), saturation: hueValue, level: device.currentValue('level')]
     parent.setColor(device.deviceNetworkId, parameters)
+    preStage()
 }
 
 def setSaturation(saturationValue){
+    // Update only the saturation of a device
+    
+    clamp(saturationValue)
+    
+    logDescriptionText("Saturation set to ${saturationValue}")
+
+
     sendEvent(name: "saturation", value: saturationValue.toFloat())
     def parameters = [hue: device.currentValue('hue'), saturation: saturationValue, level: device.currentValue('level')]
-    parent.setColor(device.deviceNetworkId, parameters)
-
-
-    
+    parent.setColor(device.deviceNetworkId, parameters) 
+    preStage()
 }
 
 def setColor(parameters){
-    sendEvent(name: "hue", value: parameters.hue.toFloat())
-    sendEvent(name: "saturation", value: parameters.saturation.toFloat())
-    sendEvent(name: "level", value: parameters.level.toInteger())
+    // Update the color of a device 
+    
+    setParameters = [:]
+    
+    if(parameters.hue){setParameters.hue = clamp(parameters.hue)
+    } else{ setParameters.hue = device.currentValue("hue")}
+    
+    if(parameters.saturation){setParameters.saturation = clamp(parameters.saturation)
+    } else{ setParameters.saturation = device.currentValue("saturation") }
+    
+    if(parameters.level){setParameters.level = clamp(parameters.level)
+    } else{ setParameters.level = device.currentValue("level") }
 
-    parent.setColor(device.deviceNetworkId, parameters)
+    sendEvent(name: "hue", value: setParameters.hue.toFloat())
+    sendEvent(name: "saturation", value: setParameters.saturation.toFloat())
+    sendEvent(name: "level", value: setParameters.level.toInteger())
+
+    parent.setColor(device.deviceNetworkId, setParameters)
+    preStage()
 }
-def setColorTemperature(colorTemperature = device.currentValue('colorTemperature'), level = device.currentValue('level')){
-    sendEvent(name: "colorTemperature", value: colorTemperature.toInteger())
-    parent.setColorTemperature(device.deviceNetworkId, colorTemperature.toInteger(), level)
+
+
+def setColorTemperature(colorTemperature = device.currentValue('colorTemperature'), levelValue = device.currentValue('level')){
+    // Update the colorTemperature of a device
+    
+    clamp(colorTemperature.toInteger(), 1000, 8000)
+    clamp(levelValue)
+    
+    sendEvent(name: "colorTemperature", value: colorTemperature)
+    parent.setColorTemperature(device.deviceNetworkId, colorTemperature, levelValue)
+    preStage()
 }
 
 def initialize(){
     // Do nothing
+    
 }
 
 def updated(){
     // Do nothing
+    
 }
 
+def refresh(){
+    // Request an info packet from the gateway
+    
+    parent.refresh()
+}
+
+def preStage(){
+    // Turn on a light when values change if this setting is false
+    
+    settings.enableColorPrestaging ? null : ( device.currentValue("switch") == "off" ? on() : null )
+}
+
+def logDescriptionText(text){
+    // Log device changes if set to
+    
+    settings.logDescriptionText == true ? log.info("${device.deviceNetworkId}: ${text}") : null   
+}
+
+def clamp( value, lowerBound = 0, upperBound = 100 ){
+    // Takes a value and ensures it's between two defined thresholds
+
+    value == null ? value = upperBound : null
+
+    if(lowerBound < upperBound){
+        if(value < lowerBound ){ value = lowerBound }
+        if(value > upperBound){ value = upperBound }
+    }
+    else if(upperBound < lowerBound){
+        if(value < upperBound){ value = upperBound }
+        if(value > lowerBound ){ value = lowerBound }
+    }
+
+    return value
+}
