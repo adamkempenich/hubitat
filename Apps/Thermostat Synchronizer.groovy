1965621c073fdc4f6eb01712f28a542f75d175c3
Adam Kempenich <skiieraj@gmail.com>
2022-06-14 17:40:03 -0500

    Update Thermostat Synchronizer.groovy

--- Apps/Thermostat Synchronizer.groovy
+++ Apps/Thermostat Synchronizer.groovy
@@ -54,26 +54,26 @@ def initialize() {
 def thermostatChanged(event){
 
     log.trace "Event: ${event}"
-    log.trace "Device Name: ${event.device.name}"
+    log.trace "Device Name: ${event.device.deviceNetworkId}"
     log.trace "Event Name: ${event.name}"
     log.trace "Event Value: ${event.value}"
 
     if(event.name == "thermostatSetpoint"){
 
         for(comparisonDevice in thermostats){
-            log.trace "Checking ${comparisonDevice.name}"
+            log.trace "Checking ${comparisonDevice.deviceNetworkId}"
 
-            if(event.device.name != comparisonDevice.name){
-                log.trace "${event.device.name} != ${comparisonDevice.name}"
+            if(event.device.deviceNetworkId != comparisonDevice.deviceNetworkId){
+                log.trace "${event.device.deviceNetworkId} != ${comparisonDevice.deviceNetworkId}"
 
-                if(comparisonDevice.hasAttribute("thermostatSetpoint")){
+                if(comparisonDevice.hasCommand("setThermostatSetpoint")){
 
                     log.trace "Device has attribute: thermostatSetpoint"
 
                     if(event.value != comparisonDevice.thermostatSetpoint){
                         log.trace "Setting thermostatSetpoint to ${event.value}"
 
-                        comparisonDevice.setThermostatSetpoint(event.value)
+                        comparisonDevice.setThermostatSetpoint(event.value.toFloat())
 
                     }
                 }
@@ -84,19 +84,19 @@ def thermostatChanged(event){
     if(event.name == "coolingSetpoint"){
 
         for(comparisonDevice in thermostats){
-            log.trace "Checking ${comparisonDevice.name}"
+            log.trace "Checking ${comparisonDevice.deviceNetworkId}"
 
-            if(event.device.name != comparisonDevice.name){
-                log.trace "${event.device.name} != ${comparisonDevice.name}"
+            if(event.device.deviceNetworkId != comparisonDevice.deviceNetworkId){
+                log.trace "${event.device.deviceNetworkId} != ${comparisonDevice.deviceNetworkId}"
 
-                if(comparisonDevice.hasAttribute("coolingSetpoint")){
+                if(comparisonDevice.hasCommand("setCoolingSetpoint")){
 
                     log.trace "Device has attribute: coolingSetpoint"
 
                     if(event.value != comparisonDevice.coolingSetpoint){
                         log.trace "Setting cooling setpoint to ${event.value}"
 
-                        comparisonDevice.setCoolingSetpoint(event.value)
+                        comparisonDevice.setCoolingSetpoint(event.value.toFloat())
 
                     }
                 }
@@ -108,19 +108,19 @@ def thermostatChanged(event){
     if(event.name == "heatingSetpoint"){
 
         for(comparisonDevice in thermostats){
-            log.trace "Checking ${comparisonDevice.name}"
+            log.trace "Checking ${comparisonDevice.deviceNetworkId}"
 
-            if(event.device.name != comparisonDevice.name){
-                log.trace "${event.device.name} != ${comparisonDevice.name}"
+            if(event.device.deviceNetworkId != comparisonDevice.deviceNetworkId){
+                log.trace "${event.device.deviceNetworkId} != ${comparisonDevice.deviceNetworkId}"
 
-                if(comparisonDevice.hasAttribute("heatingSetpoint")){
+                if(comparisonDevice.hasCommand("setHeatingSetpoint")){
 
-                    log.trace "Device has attribute: heatingSetpoint"
+                    log.trace "Device has attribute: heatingSetpoint and received value of ${event.value}"
 
                     if(event.value != comparisonDevice.heatingSetpoint){
                         log.trace "Setting heating setpoint to ${event.value}"
 
-                        comparisonDevice.setHeatingSetpoint(event.value)
+                        comparisonDevice.setHeatingSetpoint(event.value.toFloat())
 
                     }
                 }
@@ -132,12 +132,12 @@ def thermostatChanged(event){
     if(event.name == "thermostatFanMode"){
 
         for(comparisonDevice in thermostats){
-            log.trace "Checking ${comparisonDevice.name}"
+            log.trace "Checking ${comparisonDevice.deviceNetworkId}"
 
-            if(event.device.name != comparisonDevice.name){
-                log.trace "${event.device.name} != ${comparisonDevice.name}"
+            if(event.device.deviceNetworkId != comparisonDevice.deviceNetworkId){
+                log.trace "${event.device.deviceNetworkId} != ${comparisonDevice.deviceNetworkId}"
 
-                if(comparisonDevice.hasAttribute("thermostatFanMode")){
+                if(comparisonDevice.hasCommand("setThermostatFanMode")){
 
                     log.trace "Device has attribute: thermostatFanMode"
 
@@ -154,12 +154,12 @@ def thermostatChanged(event){
     if(event.name == "thermostatMode"){
 
         for(comparisonDevice in thermostats){
-            log.trace "Checking ${comparisonDevice.name}"
+            log.trace "Checking ${comparisonDevice.deviceNetworkId}"
 
-            if(event.device.name != comparisonDevice.name){
-                log.trace "${event.device.name} != ${comparisonDevice.name}"
+            if(event.device.deviceNetworkId != comparisonDevice.deviceNetworkId){
+                log.trace "${event.device.deviceNetworkId} != ${comparisonDevice.deviceNetworkId}"
 
-                if(comparisonDevice.hasAttribute("thermostatMode")){
+                if(comparisonDevice.hasCommand("setThermostatMode")){
 
                     log.trace "Device has attribute: thermostatMode"
 
@@ -183,41 +183,41 @@ def syncFromMaster(){
     for(comparisonDevice in thermostats){
         log.trace "Checking ${comparisonDevice}"
 
-        if(masterDevice.name != comparisonDevice.name){
+        if(masterDevice.deviceNetworkId != comparisonDevice.deviceNetworkId){
             log.trace "Names are different"
 
-            if(comparisonDevice.hasAttribute("thermostatSetpoint")){      
+            if(comparisonDevice.hasCommand("setThermostatSetpoint")){      
                 
                 log.trace masterDevice.currentValue('thermostatSetpoint')
                 log.trace comparisonDevice.currentValue('thermostatSetpoint')
                 
                 if(masterDevice.currentValue('thermostatSetpoint') != comparisonDevice.currentValue('thermostatSetpoint')){
 
-                    comparisonDevice.setThermostatSetpoint(masterDevice.currentValue('thermostatSetpoint'))
+                    comparisonDevice.setThermostatSetpoint(masterDevice.currentValue('thermostatSetpoint').toFloat())
                 }
             }
 
-            if(comparisonDevice.hasAttribute("coolingSetpoint")){                
+            if(comparisonDevice.hasCommand("setCoolingSetpoint")){                
                 if(masterDevice.currentValue('coolingSetpoint') != comparisonDevice.currentValue('coolingSetpoint')){
 
-                    comparisonDevice.setCoolingSetpoint(masterDevice.currentValue('coolingSetpoint').toDecimal())
+                    comparisonDevice.setCoolingSetpoint(masterDevice.currentValue('coolingSetpoint').toFloat())
                 }
             }
 
-            if(comparisonDevice.hasAttribute("heatingSetpoint")){                
+            if(comparisonDevice.hasCommand("setHeatingSetpoint")){                
                 if(masterDevice.currentValue('heatingSetpoint') != comparisonDevice.currentValue('heatingSetpoint')){
 
-                    comparisonDevice.setHeatingSetpoint(masterDevice.currentValue('heatingSetpoint').toDecimal())
+                    comparisonDevice.setHeatingSetpoint(masterDevice.currentValue('heatingSetpoint').toFloat())
                 }
             }
-            if(comparisonDevice.hasAttribute("thermostatFanMode")){                
+            if(comparisonDevice.hasCommand("setThermostatFanMode")){                
                 if(masterDevice.currentValue('thermostatFanMode') != comparisonDevice.currentValue('thermostatFanMode')){
 
                     comparisonDevice.serThermostatFanMode(masterDevice.currentValue('thermostatFanMode'))
                 }
             }
 
-            if(comparisonDevice.hasAttribute("thermostatMode")){                
+            if(comparisonDevice.hasCommand("setThermostatMode")){                
                 if(masterDevice.currentValue('thermostatMode') != comparisonDevice.currentValue('thermostatMode')){
 
                     comparisonDevice.setThermostatMode(masterDevice.currentValue('thermostatMode'))
